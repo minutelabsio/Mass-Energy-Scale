fea1c184888b6091afbec3457fadc6cdb61317f1
Jasper Palfree <well.caffeinated@gmail.com>
2014-03-04 11:28:37 -0500

    added new images

---
+++ library/images/drawings/remove-prefix.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+DIR=$1
+
+if [ ! -d $DIR ]; then
+    echo "You must specify a directory"
+    exit 1
+fi
+
+cd $DIR
+
+for file in *; do
+	newname=$( echo "$file" | sed 's/^[_0-9 \-]*//' | sed 's/ copia//')
+	cp -v "$file" "$newname"
+done
+
+IFS=$SAVEIFS
+
